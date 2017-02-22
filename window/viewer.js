c0609f1511d7fbd98d08b8e55412c1510a19c118
Knikolskiy <glucksistemi@gmail.con>
2017-02-22 09:29:38 +0300

    some basic stuff

---
+++ window/viewer.js
@@ -0,0 +1,7 @@
+function createChoiceBlock(choice) {
+    let root = document.createElement('div');
+    root.appendChild(document.createElement('h3'));
+    root.appendChild(document.createElement('div'));
+    root.appendChild(document.createElement('div'));
+    
+}
